57d8d2ac55c2487c61e563eb737b001c9212c6d6
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-30 16:27:13 -0400

    created function timeline; created if statement to convert to military time

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function init() {
     displayDate();
+    timeline();
 };
 
+// Displaying the date at the top of the calendar page
 function displayDate(){
-    $('#currentDay').text(moment().format('MMMM Do, YYYY'));
+    $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
+}
+
+function timeline() {
+    var hour = moment().format('hh');
+    var timeOfDay = moment().format('a');
+    console.log(hour);
+    console.log(timeOfDay);
+    if (timeOfDay === 'pm' && hour !== '12') {
+        hour = parseInt(hour) + 12;
+        console.log(hour);
+    };
 }
 
 init();
