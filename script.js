bc744c25c2141b3bb98c21fe0b82e77db741532c
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-30 17:45:29 -0400

    added if statement and for loops to change css class of text areas based on current hour

--- script.js
+++ script.js
@@ -4,19 +4,28 @@ function init() {
 };
 
 // Displaying the date at the top of the calendar page
-function displayDate(){
+function displayDate() {
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 }
 
 function timeline() {
     var hour = parseInt(moment().format('hh'));
     var timeOfDay = moment().format('a');
-    console.log(hour);
-    console.log(timeOfDay);
     if (timeOfDay === 'pm' && hour !== 12) {
         hour = hour + 12;
-        console.log(hour);
     };
+
+    var container = $('.container');
+    var position = hour - 9;
+    if (hour >= 9 && hour <= 17) {
+        container.children().eq(position).find('textarea').addClass('present');
+        for (var i = 0; i < position; i++) {
+            container.children().eq(i).find('textarea').addClass('past');
+        };
+        for (var i = (position + 1); i <= 8; i++) {
+            container.children().eq(i).find('textarea').addClass('future');
+        };
+    }
 }
 
 init();
