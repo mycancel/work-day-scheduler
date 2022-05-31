21b6d95f313a9ce76529ab46ef92d98657021e1c
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-31 16:00:39 -0400

    removed if and else if statements from timeline function

--- script.js
+++ script.js
@@ -16,31 +16,20 @@ function timeline() {
 
     var container = $('.container');
     var position = hour - 9;
-    // Within the hours of the calender, 
-    if (hour >= 9 && hour <= 17) {
-        // The textbox of the current hour will have the present class.
-        container.children().eq(position).find('textarea').addClass('present');
-        for (var i = 0; i < position; i++) {
-            // The textbox of past hours will have the past class.
-            container.children().eq(i).find('textarea').addClass('past');
-        };
-        for (var i = (position + 1); i <= 8; i++) {
-            // The textbox of future hours will have the future class.
-            container.children().eq(i).find('textarea').addClass('future');
-        };
-    } else if (hour < 9) {
-        // When the hour is before the hours of the calender,
-        // All text containers in the calender will have the future class.
-        container.find('textarea').addClass('future');
-    } else {
-        // When the hour is after the hours of the calender,
-        // All text containers in the calender will have the past class.
-        container.find('textarea').addClass('past');
-    }
+    // The textbox of the current hour will have the present class.
+    container.children().eq(position).find('textarea').addClass('present');
+    for (var i = 0; i < position; i++) {
+        // The textbox of past hours will have the past class.
+        container.children().eq(i).find('textarea').addClass('past');
+    };
+    for (var i = (position + 1); i <= 8; i++) {
+        // The textbox of future hours will have the future class.
+        container.children().eq(i).find('textarea').addClass('future');
+    };
 }
 
 
-$('.saveBtn').on('click', function(event) {
+$('.saveBtn').on('click', function (event) {
     var textIndex = $(this).parent().index();
     var textValue = $(this).parent().find('textarea').val();
 
